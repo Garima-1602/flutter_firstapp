e6aad013f014ba0df363ea21f49e446b897174f0
Garima <garima89077@gmail.com>
2022-05-29 17:31:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+//this is library which contain material design
+//material design lang developed by google and defines some guidelines for ios,web and all
+void main()//whatever we write in this will be executed
+{
+  runApp(
+    Center(
+      child:Text(
+          "Hello Flutter",
+          textDirection: TextDirection.ltr
+      ),
+    )
+
+  ) ;
+}
