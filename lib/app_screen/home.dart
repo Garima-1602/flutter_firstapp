6f437699e427eba1f3082773da6f733d8bd2c4f1
Garima <garima89077@gmail.com>
2022-06-05 19:06:17 +0530

    container widget added

--- lib/app_screen/home.dart
+++ lib/app_screen/home.dart
@@ -4,10 +4,29 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context)
   {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      child: Text("Plants",textDirection: TextDirection.ltr),
+    return Center(
+      child:Container(
+        alignment: Alignment.center,
+        color: Colors.green,
+        //width:200.0,
+        //height:100.0,
+        margin:EdgeInsets.all(15.0),
+        padding:EdgeInsets.all(40.0),
+        child: Text(
+            "KYP",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+            decoration: TextDecoration.none,
+           color: Colors.black,
+           fontSize:55.0,
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+
+        ),
+        ),
+      )
     );
+
+
   }
 }
